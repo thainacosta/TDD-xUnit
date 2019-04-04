c99f1e0dd43f77cde8080f702890616477c586f4
Thaina Ribeiro <thainaribeiro96.tr@gmail.com>
2019-04-04 17:25:36 -0300

    Refatorando testes

--- Alura.LeilaoOnline.Core/Leilao.cs
+++ Alura.LeilaoOnline.Core/Leilao.cs
@@ -27,16 +27,18 @@ namespace Alura.LeilaoOnline.Core
             Estado = EstadoLeilao.LeilaoEmAndamento;
         }
 
+        private bool NovoLanceEhAceito(Interessada cliente, double valor)
+        {
+            return (Estado == EstadoLeilao.LeilaoEmAndamento)
+                && (cliente != _ultimoCliente);
+        }
+
         public void RecebeLance(Interessada cliente, double valor)
         {
-            if (Estado == EstadoLeilao.LeilaoEmAndamento)
+            if (NovoLanceEhAceito(cliente, valor))
             {
-                if (cliente != _ultimoCliente)
-                {
-                    _lances.Add(new Lance(cliente, valor));
-                    _ultimoCliente = cliente;
-                }
-                
+                _lances.Add(new Lance(cliente, valor));
+                _ultimoCliente = cliente;
             }
             
         }
