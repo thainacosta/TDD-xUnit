8ce5f8e9650b526bed3eafc23aead605ee9f3fc0
Thaina Ribeiro <thainaribeiro96.tr@gmail.com>
2019-04-03 23:51:23 -0300

    Introduçao ao uso do xUnit

---
+++ Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+
+        public static void Verifica(double esperado, double obtido)
+        {
+            if (esperado == obtido)
+            {
+                Console.WriteLine("Teste Ok");
+                Console.Read();
+               
+            }
+            else
+            {
+                Console.WriteLine(
+                    $"Teste falhou!! Esperado: {esperado}, Obtido: {obtido}");
+                
+            }
+
+        }
+        public static void LeilaoComVariosLances()
+        {
+            //Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var Maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(Maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(Maria, 805);
+
+            //Act
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = (leilao.Ganhador.Valor);
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        public static void LeilaoComUmLance()
+        {
+            //Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);            
+
+            leilao.RecebeLance(fulano, 800);            
+
+            //Act
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 800;
+            var valorObtido = (leilao.Ganhador.Valor);
+            Verifica(valorEsperado, valorObtido);
+
+        }
+
+        static void Main()
+        {
+            LeilaoComVariosLances();
+            LeilaoComUmLance();
+
+        }
+    }
+}
---
+++ Alura.LeilaoOnline.testes/LeilaoTestes.cs
@@ -0,0 +1,48 @@
+﻿using Alura.LeilaoOnline.Core;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Testes
+{
+    public class LeilaoTestes
+    {
+        [Fact]
+        public void LeilaoComVariosLances()
+        {
+            //Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var Maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(Maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(Maria, 805);
+
+            //Act
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = (leilao.Ganhador.Valor);
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+        [Fact]
+        public void LeilaoComUmLance()
+        {
+            //Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            //Act
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 800;
+            var valorObtido = (leilao.Ganhador.Valor);
+            Assert.Equal(valorEsperado, valorObtido);
+
+        }
+    }
+}
